cf7ad1af62bb9b9458e81c9a7681896fc93a29ab
Sohail <ahmadsohailmaghsoudi@gmail.com>
2023-06-05 23:19:20 +0430

    creating an imaginary user telling him to find a gym

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -18,7 +17,6 @@ class MyApp extends StatelessWidget {
 
 class RootPage extends StatefulWidget {
   const RootPage({Key? key}) : super(key: key);
-
   @override
   State<RootPage> createState() => _RootPageState();
 }
@@ -27,8 +25,48 @@ class _RootPageState extends State<RootPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Herat gym system'),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Hello Amir',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Find your Gym',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: Colors.black,
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
