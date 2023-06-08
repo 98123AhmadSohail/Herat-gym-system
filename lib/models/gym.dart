bcdc339939aba3ffe1151e23bcb599f8ca3ae91d
farhad <farhadmohammadi5058@gmail.com>
2023-06-07 21:39:17 -0700

    adding model folder and putting gym file in it

---
+++ lib/models/gym.dart
@@ -0,0 +1,49 @@
+class Gym{
+
+  string imageUrl;
+  string title;
+  string description;
+  int price;
+  double rating;
+
+
+  Gym ({
+    this.description,
+    this.imageUrl,
+    this.price,
+    this.rating,
+    this.title,
+
+
+});
+}
+final List<Gym>Gyms =[
+  Gym(
+    imageUrl: 'assets/images/gm1.jpg',
+    title: 'herat Gym',
+    description: 'all Gym in herat ,city',
+    price: 100,
+    rating: 4.5,
+  ),
+  Gym(
+    imageUrl: 'assets/images/gm2.jpg',
+    title: 'herat Gym',
+    description: 'all Gym in herat ,city',
+    price: 100,
+    rating: 4.5,
+  ),
+  Gym(
+    imageUrl: 'assets/images/gm1.jpg',
+    title: 'herat Gym',
+    description: 'all Gym in herat ,city',
+    price: 100,
+    rating: 4.5,
+  ),
+  Gym(
+    imageUrl: 'assets/images/gm2.jpg',
+    title: 'herat Gym',
+    description: 'all Gym in herat ,city',
+    price: 100,
+    rating: 4.5,
+  ),
+]
